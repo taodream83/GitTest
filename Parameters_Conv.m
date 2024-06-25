9bf6e360fbe4b20fd52a91ee1161cfbab2ed3815
Gabriele <g.liga@tue.nl>
2024-06-25 12:01:24 +0200

    changed dec type.

--- Parameters_Conv.m
+++ Parameters_Conv.m
@@ -8,7 +8,7 @@ poly=[5,7];           %[13,15];   % [5,7];   % Set of polynomials defining convo
 trellis=poly2trellis(K,poly,17);                        % Trellis structure
 r=size(poly,1)/size(poly,2);    % Convolutional code rate 
 tb=1e5;                         % Decoding window
-DecType='unquant';
+DecType='quant';
 DecMode='trunc';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
